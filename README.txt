6f40a48cfc6b108b34ccc6669fa6e3ba573c6af1
Sergey Tsyrkin <sergeitsyrkin@yandex.ru>
2024-07-24 17:28:01 +0300

    new_commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+test_add_new_book_add_two_books - тестируем добавление новых книг
+test_set_book_genre - тестируем установку книге жанра
+test_get_book_genre - тестируем получение жанра книги по её имени
+test_get_books_with_specific_genre - тестируем вывод списка книг с определённым жанром
+test_get_books_genre - тестируем получение словаря books_genre
+test_get_books_for_children - тестируем возвращение книг, подходящим детям
+test_add_book_in_favorites - тестируем добавление книги в Избранное
+test_delete_book_from_favorites - тестируем удаление книги из Избранного
+test_get_list_of_favorites_books - тестируем получение списка Избранных книг
--- tests.py
+++ tests.py
@@ -1,24 +1,85 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    # тестируем добавление новых книг
+    @pytest.mark.parametrize('name', ['Тёмная башня', 'Оно'])
+    def test_add_new_book_add_two_books(self, name):
         collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name in collector.get_books_genre()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    # тестируем установку книге жанра
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная башня')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Тёмная башня', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        assert collector.get_book_genre('Тёмная башня') == 'Фантастика'
+        assert collector.get_book_genre('Оно') == 'Ужасы'
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    # тестируем получение жанра книги по её имени
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Тёмная Башня")
+        genre = "Фантастика"
+        collector.set_book_genre("Тёмная Башня", genre)
+        assert collector.get_book_genre("Тёмная Башня") == genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # тестируем вывод списка книг с определённым жанром
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Оно')
+        collector.add_new_book('Тёмная башня')
+        collector.set_book_genre('Оно', 'Ужасы')
+        collector.set_book_genre('Тёмная башня', 'Фантастика')
+        books = collector.get_books_with_specific_genre('Ужасы')
+        assert 'Оно' in books
+        assert 'Тёмная башня' not in books
+
+    # тестируем получение словаря books_genre
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная башня')
+        collector.add_new_book('Оно')
+        assert 'Тёмная башня' in collector.get_books_genre()
+        assert 'Оно' in collector.get_books_genre()
+
+    # тестируем возвращение книг, подходящим детям
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная Башня')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Тёмная Башня', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        children_books = collector.get_books_for_children()
+        assert 'Тёмная Башня' in children_books
+        assert 'Оно' not in children_books
+
+    # тестируем добавление книги в Избранное
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная Башня')
+        collector.add_book_in_favorites('Тёмная Башня')
+        assert 'Тёмная Башня' in collector.get_list_of_favorites_books()
+
+    # тестируем удаление книги из Избранного
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная Башня')
+        collector.add_book_in_favorites('Тёмная Башня')
+        collector.delete_book_from_favorites('Тёмная Башня')
+        assert 'Тёмная Башня' not in collector.get_list_of_favorites_books()
+
+    # тестируем получение списка Избранных книг
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Тёмная Башня')
+        collector.add_new_book('Оно')
+        collector.add_book_in_favorites('Тёмная Башня')
+        collector.add_book_in_favorites('Оно')
+        favorites = collector.get_list_of_favorites_books()
+        assert 'Тёмная Башня' in favorites
+        assert 'Оно' in favorites
